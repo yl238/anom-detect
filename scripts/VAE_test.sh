fa9d8f2e7dc15f46734e34389a83d3f33ef4ca37
Sue Liu <sue.liu@mudano.com>
2018-11-09 15:39:50 +0000

    VAE test now include average precision, remove cuda

--- scripts/VAE_test.sh
+++ scripts/VAE_test.sh
@@ -9,7 +9,7 @@ export DATA_DIR='../data/tar_dir/'
 export EXP_DIR='../exp_results/'
 
 # test
-$PYTHONBIN ../outlier_detection.py --cuda \
+$PYTHONBIN ../outlier_detection.py \
     --data ${DATA_DIR} \
     --model_path ${EXP_DIR}/NORMAL0.1/best_model.pth.tar \
     --image_size 128 --kl_weight 0.01 --out_csv auc_result_auc.csv
