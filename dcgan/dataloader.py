478e4437ceb1f2e250c2d3132f33d35e21077a2a
Sue Liu <sue.liu@mudano.com>
2018-11-27 17:17:12 +0000

    Process images just as for VAE

--- dcgan/dataloader.py
+++ dcgan/dataloader.py
@@ -6,14 +6,13 @@ from torchvision import datasets, transforms
 def load_train_dataset(image_size, dataroot, batch_size=1):
     dataset = datasets.ImageFolder(root=dataroot,
         transform=transforms.Compose([
-            transforms.Resize(image_size),
-            transforms.CenterCrop(image_size),
+            transforms.Resize((image_size, image_size)),
             transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
         ]))
 
     # Create the dataloader
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
-        shuffle=True, num_workers=4)
+        shuffle=True, num_workers=4, pin_memory=True)
 
     return dataloader
--- dcgan/main.py
+++ dcgan/main.py
@@ -24,3 +24,18 @@ print("Random seed: ", MANUAL_SEED)
 random.seed(MANUAL_SEED)
 torch.manual_seed(MANUAL_SEED)
 
+from dataloader import load_train_dataset
+
+data_dir = '/Users/sueliu/projects/anom-detect/data/tar_dir/vae_train'
+loader = load_train_dataset(128, data_dir, batch_size=64)
+
+# Decide which device we want to run on
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+real_batch = next(iter(loader))
+plt.figure(figsize=(15, 15))
+plt.axis('off')
+plt.title('Training images')
+plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64],
+padding=2, normalize=True).cpu(), (1, 2, 0)))
+plt.savefig('test.png', bbox_inches='tight')
